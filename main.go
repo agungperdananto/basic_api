c8821bd484061150aa92f25b431064b8467a5a96
Agung  Perdananto <agung.perdananto@sirclo.com>
2023-09-30 22:22:03 +0700

    feat [test]: add http request example

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+
 	"github.com/agungperdananto/basic_api/controllers"
 	"github.com/agungperdananto/basic_api/initializers"
 	"github.com/gin-gonic/gin"
@@ -20,5 +23,30 @@ func main() {
 	r.GET("/posts/:id", controllers.PostShow)
 
 	r.DELETE("/posts/:id", controllers.PostDelete)
+
+	// Test request
+	r.GET("/direct", func(c *gin.Context) {
+		response, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
+		if err != nil {
+			c.Status(http.StatusServiceUnavailable)
+			return
+		}
+
+		// if there was no error, you should close the body
+		defer response.Body.Close()
+
+		// hence this condition is moved into its own block
+		if response.StatusCode != http.StatusOK {
+			c.Status(http.StatusServiceUnavailable)
+			return
+		}
+
+		// use a proper struct type in your real code
+		// the empty interface is just for demonstration
+		var v interface{}
+		json.NewDecoder(response.Body).Decode(&v)
+
+		c.JSON(200, v)
+	})
 	r.Run()
 }
